572f9547ec76a96cdb59975544b254f76dd0bd70
Tsuyoshi Sadakata <sadapon2008@gmail.com>
2016-09-13 15:02:02 +0900

    MySQL実験

--- app/Config/database.php
+++ app/Config/database.php
@@ -2,19 +2,21 @@
 class DATABASE_CONFIG {
 
         public $default = array(
-                'datasource' => 'Database/Postgres',
+                'datasource' => 'Database/MySql',
                 'persistent' => false,
                 'host' => 'localhost',
-                'login' => 'postgres',
-                'password' => 'postgres',
+                'login' => 'root',
+                'password' => 'root',
                 'database' => 'cake',
+                'encoding' => 'utf8',
         );
         public $test = array(
-                'datasource' => 'Database/Postgres',
+                'datasource' => 'Database/MySql',
                 'persistent' => false,
                 'host' => 'localhost',
-                'login' => 'postgres',
-                'password' => 'postgres',
+                'login' => 'root',
+                'password' => 'root',
                 'database' => 'test_cake',
+                'encoding' => 'utf8',
         );
 }
